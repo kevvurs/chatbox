91e609e2a33a42ab74e1ff7091f72e7f111e4181
Kevin <kevin@seedshare.io>
2025-05-17 14:55:55 -0500

    implement interactivity

--- chatbox/main.go
+++ chatbox/main.go
@@ -2,25 +2,33 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"syscall/js"
 
 	"github.com/kevvurs/chatbox/html"	
 )
 
+
+var document js.Value;
+var container js.Value;
+var chatWindow js.Value;
+var chat js.Value;
+var textEntry js.Value;
+
 func main() {
 	fmt.Println("Loading chatbox v1.0")
 
 	// create a floating icon to open the chat when idle
-	document := js.Global().Get("document")
-	container := html.CreateDiv(document, "fixed-bottom")
-	openChatButton := html.CreateButton(document, "btn btn-light btn-lg btn-circle")
-	icon := html.CreateItalic(document, "glyphicon glyphicon-comment", "")
+	document = js.Global().Get("document")
+	container = html.CreateDiv(document, "fixed-bottom openchat collapse show")
+	openChatButton := html.CreateButton(document, "btn btn-light btn-lg btn-circle p-2")
+	icon := html.CreateItalic(document, "bi bi-chat-fill", "")
 
 	html.Append(openChatButton, icon)
 	html.Append(container, openChatButton)
 
 	// create a floating window for chat
-	chatWindow := html.CreateDiv(document, "fixed-bottom chatwindow")
+	chatWindow = html.CreateDiv(document, "fixed-bottom chatwindow collapse")
 
 	menubar := html.CreateDiv(document, "d-flex justify-content-between " +
 		"align-items-center bg-light p-1")
@@ -32,11 +40,11 @@ func main() {
 	html.Append(dismissButton, dismissIcon)
 	html.Append(menubar, dismissButton)
 
-	chat := html.CreateDiv(document, "bg-light p-1 border " +
-		"border-start-0 border-end-0 chatstream")
+	chat = html.CreateDiv(document, "d-flex flex-column bg-light p-1 " +
+		"border border-start-0 border-end-0 overflow-auto chatstream")
 
 	sendbar := html.CreateDiv(document, "input-group bg-light px-1 py-2")
-	textEntry := html.CreateInput(document, "text", "form-control bg-light " +
+	textEntry = html.CreateInput(document, "text", "form-control bg-light " +
 		"border-1 border-secondary focus-ring focus-ring-secondary")
 	textEntry.Set("placeholder", "Aa")
 	sendChatButton := html.CreateButton(document, "btn btn-outline-secondary")
@@ -50,7 +58,51 @@ func main() {
 	html.Append(chatWindow, sendbar)
 
 	// initialize click handlers
-	
+	textEntry.Call("addEventListener", "keydown", js.FuncOf(sendChatOnEnter))
+	sendChatButton.Call("addEventListener", "click", js.FuncOf(sendChat))
+	openChatButton.Call("addEventListener", "click", js.FuncOf(openChat))
+	dismissButton.Call("addEventListener", "click", js.FuncOf(dismissChat))
+
 	document.Get("body").Call("appendChild", chatWindow)
+	document.Get("body").Call("appendChild", container)
 	fmt.Println("chatbox loaded")
+	<-make(chan bool)
+}
+
+func sendChat(this js.Value, args []js.Value) any {
+	message := textEntry.Get("value").String()
+	if strings.TrimSpace(message) == "" {
+		return ""
+	}
+	messageBubble := html.CreateParagraph(document, "bg-primary p-1 mw-75 align-self-end " +
+		"flex-shrink-0 text-light rounded shadow-lg",	message)
+	html.Append(chat, messageBubble)
+	chat.Set("scrollTop", chat.Get("scrollHeight"))
+	textEntry.Set("value", "")
+	return ""
+}
+
+func sendChatOnEnter(this js.Value, args []js.Value) any {
+	if len(args) > 0 && args[0].Get("key").String() != "Enter" {
+		return ""
+	}
+	return sendChat(this, args)
+}
+
+
+func openChat(this js.Value, args []js.Value) any {
+  openClassList := container.Get("classList")
+	openClassList.Call("remove", "show")
+	chatClassList := chatWindow.Get("classList")
+	chatClassList.Call("add", "show")
+	return ""
+}
+
+
+func dismissChat(this js.Value, args []js.Value) any {
+	classList := chatWindow.Get("classList")
+	classList.Call("remove", "show")
+	openClassList := container.Get("classList")
+	openClassList.Call("add", "show")
+	return ""
 }
